988de383be3412f48414811dd293d6b36603592b
Adnan Vatric <vatric_adnan@hotmail.com>
2020-04-22 05:17:01 +0200

    add idea folder to gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,9 @@
         <div class="hero-body">
             <div class="container">
                 <h1 class="title">
-                    Development
+                    {{ env('APP_NAME', 'Sample') }}
                 </h1>
-                <h2 class="subtitle">
-                    This is a sample how you can create a simple web application
-                </h2>
             </div>
         </div>
     </section>
-
 @endsection
